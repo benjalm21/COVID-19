756b92d5801e3f4897ed1ac3c64c6d560bca502f
benjalm21 <82690716+benjalm21@users.noreply.github.com>
2021-04-23 11:59:13 -0700

    Se agrego la descarga de archivos auxiliares
    Se descargaron los descriptores y catalogos.

--- covid19.sh
+++ covid19.sh
@@ -1,9 +1,17 @@
 mkdir covid19
 cd covid19
 curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
+curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/diccionario_datos_covid19.zip
+
+unzip diccionario_datos_covid19.zip.zip && \
+rm rm datos_abiertos_covid19.zip && \ && \
+
 unzip datos_abiertos_covid19.zip && \
 rm datos_abiertos_covid19.zip && \
+
 mv *COVID19MEXICO.csv virgindata.csv
+mv *Catalogos.xlsx Catalogos.xlsx
+mv *Descriptores_.xlsx Descriptores.xlsx
 csvcut -c 36,8,6,9,13,16,21 virgindata.csv > filteredcolumns.csv
 csvgrep -c 2 -m "26" filteredcolumns.csv | csvgrep -c 1 -r "[123]" | csvgrep -c 7 -r "[1]" > filteredrows.csv
 csvgrep -i -c 5 -m "9999-99-99" filteredrows.csv > final_defunciones_diabetes_covid.csv 
